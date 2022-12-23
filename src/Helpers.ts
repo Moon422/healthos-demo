75c27e0ae5255f8e7646085e5916fe1cddc7e4d8
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2022-12-23 19:48:29 +0600

    live backend url added

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -1,6 +1,6 @@
 import { User } from "./App";
 
-export const HOST_URL = "http://localhost:8000";
+export const HOST_URL = "https://6av79h.deta.dev";
 
 export async function verifyUser(token: string, callback: (u: User) => void) {
     const response = await fetch(`${HOST_URL}/verify`, {
@@ -23,4 +23,24 @@ export async function verifyUser(token: string, callback: (u: User) => void) {
     } else {
         alert("Token not verfied");
     }
+}
+
+export async function loadProducts(token: string) {
+    const url = `${HOST_URL}/products`;
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    });
+
+    if (response.ok) {
+        const products = await response.json();
+
+        if (products) {
+            return products;
+        }
+    }
+
+    throw new Error("Error loading products");
 }
