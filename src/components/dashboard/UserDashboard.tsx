75d1b73eb49711e831bd44065bc69bd717401fd5
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2022-12-24 17:50:29 +0600

    add to cart

--- src/components/dashboard/UserDashboard.tsx
+++ src/components/dashboard/UserDashboard.tsx
@@ -1,5 +1,5 @@
 import { url } from "inspector";
-import { Component, ReactNode, MouseEvent as ReactMouseEvent } from "react";
+import { Component, ReactNode, MouseEvent as ReactMouseEvent, FormEvent } from "react";
 import { User } from "../../App";
 import { loadProducts } from "../../Helpers";
 
@@ -62,10 +62,26 @@ export type Product = {
     image: string
 };
 
-class ProductDetailsView extends Component<{ product: Product, backBtnCallback: () => void }> {
+class ProductDetailsView extends Component<{ product: Product, backBtnCallback: () => void, onProductAddToCart: (item: CartItem) => void }> {
     onBackClicked(e: ReactMouseEvent<HTMLButtonElement, MouseEvent>): void {
         e.preventDefault();
         this.props.backBtnCallback();
+    }
+
+    onFormSubmit(e: FormEvent<HTMLFormElement>): void {
+        e.preventDefault();
+
+        const formdata = new FormData(e.currentTarget);
+
+        const quanity = parseInt(formdata.get("quantity") as string);
+        if (isNaN(quanity) || quanity <= 0) {
+            return;
+        }
+
+        this.props.onProductAddToCart({
+            product: this.props.product,
+            quanity: quanity
+        });
     }
 
     render(): ReactNode {
@@ -80,10 +96,10 @@ class ProductDetailsView extends Component<{ product: Product, backBtnCallback: 
                         <p>{this.props.product.description}</p>
                         <p>Instock: {this.props.product.quantity}</p>
                         <p>Price: ${this.props.product.price}</p>
-                        <form className="flex space-x-4 my-2">
+                        <form onSubmit={e => this.onFormSubmit(e)} className="flex space-x-4 my-2">
                             <input type="number" name="quantity" id="quantity" className="rounded border-slate-300 border-2 p-1" placeholder="Quanity" />
                             <button type="submit" className="h-10 px-6 font-semibold rounded-md bg-black text-white hover:ring-4">Add to Cart</button>
-                            <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900 hover:ring-4">Buy Now</button>
+                            <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900 hover:ring-4 hidden">Buy Now</button>
                         </form>
                     </div>
                 </div>
@@ -116,7 +132,7 @@ class ProductListItem extends Component<{ product: Product, clickCallback: (prod
     }
 }
 
-class ProductList extends Component<{ user: User }, { products: Product[], selectedProduct?: Product }> {
+class ProductList extends Component<{ user: User, onProductAddToCart: (item: CartItem) => void }, { products: Product[], selectedProduct?: Product }> {
     state: { products: Product[], selectedProduct?: Product } = {
         products: []
     }
@@ -148,7 +164,8 @@ class ProductList extends Component<{ user: User }, { products: Product[], selec
 
     render(): ReactNode {
         if (this.state.selectedProduct) {
-            return <ProductDetailsView product={this.state.selectedProduct} backBtnCallback={() => this.showProductDetails(undefined)} />
+            return <ProductDetailsView product={this.state.selectedProduct} backBtnCallback={() => this.showProductDetails(undefined)}
+                onProductAddToCart={item => this.props.onProductAddToCart(item)} />
         }
 
         return (
@@ -163,9 +180,43 @@ class ProductList extends Component<{ user: User }, { products: Product[], selec
     }
 }
 
-export class UserDashboard extends Component<{ user: User, logoutCallback: () => void }, { currentView: MenuItems }> {
-    state: { currentView: MenuItems } = {
-        currentView: MenuItems.PRODUCTS
+type CartItem = {
+    product: Product,
+    quanity: number
+};
+
+class CartItemView extends Component<{ cartItem: CartItem }> {
+    render(): ReactNode {
+        return (
+            // <h1>{`${this.props.cartItem.product.name} ${this.props.cartItem.quanity}`}</h1>
+            <>
+                <div className="col-span-8 border-2 border-t-0 border-slate-700 p-2">{this.props.cartItem.product.name}</div>
+                <div className="bg-slate-600 p-2 text-right border-b-2 border-slate-700 text-white">{this.props.cartItem.quanity}</div>
+                <div className="bg-slate-600 p-2 border-2 border-t-0 border-slate-700 text-white"><button className="btn w-full">Remove</button></div>
+            </>
+        )
+    }
+}
+
+class CartView extends Component<{ cartItems: CartItem[] }> {
+    render(): ReactNode {
+        return (
+            <div className="grid grid-cols-10 my-2">
+                <div className="text-center my-auto p-2 col-span-8 bg-slate-400 border-2 border-slate-700 rounded-tl-lg">Name of Product</div>
+                <div className="text-center my-auto p-2 bg-slate-400 border-t-2 border-b-2 border-slate-700">Quantity</div>
+                <div className="text-center my-auto p-2 bg-slate-400 border-2 border-slate-700 rounded-tr-lg">Action</div>
+                {
+                    this.props.cartItems.map((item, idx) => <CartItemView key={idx} cartItem={item} />)
+                }
+            </div>
+        );
+    }
+}
+
+export class UserDashboard extends Component<{ user: User, logoutCallback: () => void }, { currentView: MenuItems, cartItems: CartItem[] }> {
+    state: { currentView: MenuItems, cartItems: CartItem[] } = {
+        currentView: MenuItems.PRODUCTS,
+        cartItems: []
     }
 
     onLogoutBtnClicked(e: ReactMouseEvent<HTMLButtonElement, MouseEvent>): void {
@@ -180,16 +231,22 @@ export class UserDashboard extends Component<{ user: User, logoutCallback: () =>
         });
     }
 
+    addItemToCart(item: CartItem) {
+        this.setState(prevState => ({
+            cartItems: [...prevState.cartItems, item]
+        }));
+    }
+
     render(): ReactNode {
         let mainContent: JSX.Element | undefined = undefined;
 
         switch (this.state.currentView) {
             case MenuItems.PRODUCTS:
-                mainContent = <ProductList user={this.props.user} />
+                mainContent = <ProductList user={this.props.user} onProductAddToCart={item => this.addItemToCart(item)} />
                 break;
 
             default:
-                mainContent = <h1>Cart</h1>;
+                mainContent = <CartView cartItems={this.state.cartItems} />;
                 break;
         }
 
